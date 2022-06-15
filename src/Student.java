5a0d32fc8415f20a76c2bc6a50aae274d60b16ed
tttung7 <tungcsp221199@gmail.com>
2022-06-15 17:31:57 +0700

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    private int age;
+    private int year;
+
+    public Student(int age, int year) {
+        this.age = age;
+        this.year = year;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
