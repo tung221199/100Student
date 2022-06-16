5ad9f0a4eb3410953e707d8ead76258e23e04265
tttung7 <tungcsp221199@gmail.com>
2022-06-16 17:19:27 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
+
     public static void main(String[] args) {
-        
+        List<Student> students= new ArrayList<>(100);
+
+        for (int i=0; i< students.size();i++){
+            int ranAge= ThreadLocalRandom.current().nextInt(18,25);
+            int ranY= ThreadLocalRandom.current().nextInt(1,7);
+            Student student = new Student(ranAge,ranY);
+            students.add(student);
+            System.out.println("Sinh viên với tuổi: "+ student.getAge()+ " và số năm học" +student.getYear());
+        }
+
     }
 }
+
