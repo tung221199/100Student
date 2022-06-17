979de710451f52d836d7925412b7539912361292
tttung7 <tungcsp221199@gmail.com>
2022-06-17 17:36:38 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,26 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Student> students= new ArrayList<>(100);
+        List<Student> students= new ArrayList<Student>(100);
 
-        for (int i=0; i< students.size();i++){
+        for (int i=0; i< 100;i++){
             int ranAge= ThreadLocalRandom.current().nextInt(18,25);
             int ranY= ThreadLocalRandom.current().nextInt(1,7);
             Student student = new Student(ranAge,ranY);
             students.add(student);
-            System.out.println("Sinh viên với tuổi: "+ student.getAge()+ " và số năm học" +student.getYear());
+            System.out.println("Sinh viên với tuổi: "+ student.getAge()+ " và số năm học " +student.getYear());
         }
-
+        System.out.println("--------");
+        List<Student> studentList=  students.stream().sorted((a1,a2) -> a1.getAge() - a2.getAge()).limit(10)
+                .sorted((a1,a2) -> a2.getYear() - a1.getYear()).collect(Collectors.toList());
+        for (int i=0; i< 10;i++){
+            System.out.println("Sinh viên với tuổi nhỏ nhất "+ studentList.get(i).getAge()+ " và số năm học nhiều nhất " +studentList.get(i).getYear());
+        }
     }
 }
 
